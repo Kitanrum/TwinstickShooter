4510262eb9c4b8c84942046b01b0bb69c8d16582
Krystel Theuvenin <k.e.theuv@gmail.com>
2016-02-10 15:09:33 -0500

    Enemy Behaviour
    Enemy Behavior has been added with WaveSpanwer

--- Assets/Scripts/EnemyBehaviour.js
+++ Assets/Scripts/EnemyBehaviour.js
@@ -1,9 +1,45 @@
 ﻿#pragma strict
 
+import System.Collections.Generic;
+
+private var player : Transform;
+public var speed : float = 2.0;
+public var health : int = 2.0;
+
 function Start () {
+
+	player = GameObject.Find("playerShip").transform;
 
 }
 
 function Update () {
 
+	Movement();
+
+}
+
+function Movement(){
+
+	var delta : Vector3 = player.position - transform.position;
+	delta.Normalize();
+	var moveSpeed : float = speed * Time.deltaTime;
+	transform.position = transform.position + (delta * moveSpeed);
+
+}
+
+function OnCollisionEnter2D(theCollision : Collision2D){
+
+	Debug.Log("Hit" + theCollision.gameObject.name);
+
+	if(theCollision.gameObject.name.Contains("laser")){
+
+		var laser : LaserBehaviour = theCollision.gameObject.GetComponent("LaserBehaviour") as LaserBehaviour;
+		health -= laser.damage;
+		Destroy (theCollision.gameObject);
+
+	}
+
+	if(health <=0) {
+		Destroy(this.gameObject);
+	}
 }
---
+++ Assets/Scripts/GameController.js
@@ -0,0 +1,51 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+public var enemy : Transform;
+
+public var timeBeforeSpawning : float = 1.5;
+public var timeBetweenEnemies : float = .25;
+public var timeBeforeWaves : float = 2.0;
+
+public var enemiesPerWave : float = 10.0;
+private var currentNumberOfEnemies : float = 0;
+
+function Start () {
+
+	StartCoroutine(SpawnEnemies());
+
+}
+
+function Update () {
+
+}
+
+function SpawnEnemies(){
+
+	yield return new WaitForSeconds(timeBeforeSpawning);
+
+	while(true){
+
+		if(currentNumberOfEnemies <= 0){
+			var randDirection : float;
+			var randDistance : float;
+
+			for (var i : int = 0; i < enemiesPerWave; i++){
+
+				randDistance = Random.Range(10,25);
+				randDirection = Random.Range(0,360);
+
+				var posX : float = this.transform.position.x + (Mathf.Cos((randDirection) * Mathf.Deg2Rad) * randDistance);
+				var posY : float = this.transform.position.y + (Mathf.Sin((randDirection) * Mathf.Deg2Rad) * randDistance);
+
+				Instantiate(enemy, new Vector3 (posX, posY, 0), this.transform.rotation);
+				currentNumberOfEnemies++;
+
+				yield return new WaitForSeconds(timeBetweenEnemies);
+			}
+		}
+
+		yield return new WaitForSeconds(timeBeforeWaves);
+	}
+}
